89402f496ed769bd3838aa2c4321c3d83ad7d1c6
keith156 <keith156@users.noreply.github.com>
2026-07-14 17:21:16 +0300

    fix: remove unused firebase.storage() initialization which was causing crashes on public pages

--- firebase-config.js
+++ firebase-config.js
@@ -11,7 +11,6 @@ const firebaseConfig = {
 // Initialize Firebase compat
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
-const storage = firebase.storage();
 
 const dbHelper = {
   async fetchItems(collectionName, limitVal = null) {
